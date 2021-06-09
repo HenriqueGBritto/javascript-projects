9bd7623c43766ef4356e690df4267f88f69d9345
Henrique <henriquegbritto2012@gmail.com>
2021-06-09 07:42:19 -0300

    new changes

--- form-validator/script.js
+++ form-validator/script.js
@@ -7,18 +7,41 @@ const password2 = document.getElementById('password2')
 //functions
 
 //show input error message
+
 function showError(input, message){
     const formControl = input.parentElement
-    formControl.className = 'form-control error'
+    formControl.classList.add('error')
 
+    const small = formControl.querySelector('small')
+    small.innerText = message
+}
+
+//Show success
+
+function showSuccess(input){
+    const formControl = input.parentElement
+    formControl.classList.add('success')
+}
+
+//check email is valid
+function isValidEmail(email){
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase())
+}
+
+//check required fields
+
+function checkRequired(inputArr){
+    inputArr.forEach((input)=>{
+        console.log(input)
+    })
+}
 
 // event listeners
-form.addEventListener('submit', function(event){
+form.addEventListener('submit', (event) => {
     event.preventDefault()
-    
-    if(username.value === ''){
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    }
+
+    checkRequired(username)
 })
+
+
